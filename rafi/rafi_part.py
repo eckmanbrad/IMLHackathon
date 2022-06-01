5809f92f7c9157ff69456e99dae356d43ee872cc
Rafi-Levy <101130509+Rafi-Levy@users.noreply.github.com>
2022-06-02 02:33:52 +0300

    night 1

--- rafi/rafi_part.py
+++ rafi/rafi_part.py
@@ -5,31 +5,40 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-def clean(df):
-    df['NS'] = 0
-    df['EW'] = 0
-    df['NS'][np.where(df['linqmap_magvar'] < 90 or df['linqmap_magvar'] > 270)] = 1
-    df['EW'][np.where(df['linqmap_magvar'] > 90 and df['linqmap_magvar'] < 270)] = 1
-
+def clean(dirty_df):
+    # remove unwanted cols
     cols_to_remove = ['OBJECTID', 'linqmap_reportDescription', 'linqmap_nearby', 'linqmap_reportMood',
-                      'linqmap_expectedBeginDate', 'linqmap_expectedEndDate', 'nComments', 'linqmap_magvar']
+                      'linqmap_expectedBeginDate', 'linqmap_expectedEndDate', 'nComments']
+    dirty_df.drop(columns=cols_to_remove, inplace=True)
 
-    df.drop(columns=cols_to_remove)
-    return df
+    # remove nans from chosen cols
+    dirty_df.dropna(subset=['linqmap_type', 'linqmap_subtype', 'linqmap_reportRating', 'linqmap_roadType',
+                            'linqmap_magvar', 'linqmap_reliability'], inplace=True)
+
+    # add NS & WE cols
+    dirty_df['NS'] = np.where((dirty_df['linqmap_magvar'] < 90) | (dirty_df['linqmap_magvar'] > 270), 1, 0)
+    dirty_df['EW'] = np.where((dirty_df['linqmap_magvar'] > 180) | (dirty_df['linqmap_magvar'] < 361), 1, 0)
+
+    # remove magvar col
+    dirty_df.drop(columns='linqmap_magvar', inplace=True)
+    return dirty_df
 
 
 def main():
     df = pd.read_csv('waze_data.csv')
+    df = clean(df)
+    print('all ok')
+
     print(df.columns)
-    print(set(df['linqmap_city']))
-    print(set(df['linqmap_street']))
+    # print(set(df['linqmap_city']))
+    # print(set(df['linqmap_street']))
     # print(sum(df['linqmap_subtype'].isna()))
 
     cols_to_remove = ['OBJECTID', 'linqmap_reportDescription', 'linqmap_nearby', 'linqmap_reportMood',
                       'linqmap_expectedBeginDate', 'linqmap_expectedEndDate', 'nComments']
 
     subtypes = df.groupby('linqmap_type').aggregate({'linqmap_subtype': np.unique})
-    print(subtypes)
+    subtypes.to_csv('subtypes.csv')
 
     # new map
     df['str_roadType'] = df.loc[:, 'linqmap_roadType'].astype(str)
