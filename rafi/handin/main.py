9fdcea494b08112e0b70f484d4daeb99c9b6fdb6
Rafi-Levy <101130509+Rafi-Levy@users.noreply.github.com>
2022-06-02 21:53:55 +0300

    push

--- rafi/handin/main.py
+++ rafi/handin/main.py
@@ -1,8 +1,11 @@
-
+import pandas as pd
 
 def main(path_to_task_1_test, path_to_task_2_test):
-    return None
+
 
 
 if __name__ == '__main__':
-    main()
+    '''
+    05.06.2022, 07.06.2022 and 09.06.2022
+    '''
+    main('waze_take_features.csv',[pd.to_datetime()])
