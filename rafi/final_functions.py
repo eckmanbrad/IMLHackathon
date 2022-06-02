54f377c51b45386f438c65e14aaf9a5b3e6e6368
Rafi-Levy <101130509+Rafi-Levy@users.noreply.github.com>
2022-06-02 11:32:16 +0300

    clean and select tlv

---
+++ rafi/final_functions.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import numpy as np
+
+
+# clean df
+def clean(dirty_df):
+    # get tlv rows
+    dirty_df = dirty_df[dirty_df['linqmap_city'] == 'תל אביב - יפו'].copy()
+
+    # remove unwanted cols
+    cols_to_remove = ['OBJECTID', 'linqmap_reportDescription', 'linqmap_nearby', 'linqmap_reportMood',
+                      'linqmap_expectedBeginDate', 'linqmap_expectedEndDate', 'nComments', 'linqmap_city']
+    dirty_df.drop(columns=cols_to_remove, inplace=True)
+
+    # remove nans from chosen cols
+    dirty_df.dropna(subset=['linqmap_type', 'linqmap_subtype', 'linqmap_reportRating', 'linqmap_roadType',
+                            'linqmap_magvar', 'linqmap_reliability', 'linqmap_street'], inplace=True)
+
+    # add NS & WE cols
+    dirty_df['NS'] = np.where((dirty_df['linqmap_magvar'] < 90) | (dirty_df['linqmap_magvar'] > 270), 1, 0)
+    dirty_df['EW'] = np.where((dirty_df['linqmap_magvar'] < 180), 1, 0)
+
+    # remove magvar col
+    dirty_df.drop(columns='linqmap_magvar', inplace=True)
+
+    # convert date & time & timestamp to datetime object
+    dirty_df['pubDate'] = dirty_df.apply(lambda row: dt.datetime.strptime(row.pubDate, "%m/%d/%Y %H:%M:%S"), axis=1)
+    dirty_df['update_date'] = dirty_df.apply(lambda row: dt.datetime.fromtimestamp(row.update_date / 1000), axis=1)
+
+    return dirty_df
--- rafi/rafi_part.py
+++ rafi/rafi_part.py
@@ -3,39 +3,10 @@ import openpyxl
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-import datetime as dt
 
 
-# get relevant cols
-def get_relevant(df):
-    df = pd.concat([df[df['linqmap_city'] == 'תל אביב - יפו'], df['linqmap_city'] == 'רמת גן'], axis=0)
-    print(df)
 
 
-# clean df
-def clean(dirty_df):
-    # remove unwanted cols
-    cols_to_remove = ['OBJECTID', 'linqmap_reportDescription', 'linqmap_nearby', 'linqmap_reportMood',
-                      'linqmap_expectedBeginDate', 'linqmap_expectedEndDate', 'nComments', 'linqmap_city',
-                      'linqmap_street']
-    dirty_df.drop(columns=cols_to_remove, inplace=True)
-
-    # remove nans from chosen cols
-    dirty_df.dropna(subset=['linqmap_type', 'linqmap_subtype', 'linqmap_reportRating', 'linqmap_roadType',
-                            'linqmap_magvar', 'linqmap_reliability'], inplace=True)
-
-    # add NS & WE cols
-    dirty_df['NS'] = np.where((dirty_df['linqmap_magvar'] < 90) | (dirty_df['linqmap_magvar'] > 270), 1, 0)
-    dirty_df['EW'] = np.where((dirty_df['linqmap_magvar'] < 180), 1, 0)
-
-    # remove magvar col
-    dirty_df.drop(columns='linqmap_magvar', inplace=True)
-
-    # convert date & time & timestamp to datetime object
-    dirty_df['pubDate'] = dirty_df.apply(lambda row: dt.datetime.strptime(row.pubDate, "%m/%d/%Y %H:%M:%S"), axis=1)
-    dirty_df['update_date'] = dirty_df.apply(lambda row: dt.datetime.fromtimestamp(row.update_date / 1000), axis=1)
-
-    return dirty_df
 
 
 def get_comb(df):
@@ -51,11 +22,8 @@ def get_comb(df):
 
 def main():
     df = pd.read_csv('waze_data.csv')
+    # df.to_excel('wazeee.xlsx')
 
-    df.to_excel('wazeee.xlsx')
-    return
-    df = get_relevant(df)
-    print(df['linqmap_city'])
     df = clean(df)
     df.to_csv('test.csv')
     # get_comb(df)
